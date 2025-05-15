05aab610e19751cac9479c7dfc182c131508674e
Aakash Shrestha <aakashstha168@gmail.com>
2025-05-15 17:17:52 +0545

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,7 @@
+# Making Day 1 Final Project
+print("Welcome to brand name generator !!! ")
+name = input("What is your city name :  \n")
+pet_name = input("What is your pet name : \n ")
+
+
+print("Your brand name would be : " + name + " " + pet_name)
---
+++ day4.py
@@ -0,0 +1,65 @@
+import random
+from random import choice
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images = [rock, paper, scissors]
+
+user_choice = int(input("Type 0 for Rock, 1 for Paper, 2 for Scissors: "))
+print(game_images[user_choice])
+
+computer_choice = random.randint(0,2)
+print("Computer Chooses : ")
+print(game_images[computer_choice])
+
+if user_choice >= 3 or user_choice < 0 :
+    print("you typed invalid number. You lose.")
+elif user_choice == 0 and computer_choice == 2:
+    print("you win!")
+elif computer_choice == 0 and user_choice == 2:
+    print("you lose!")
+elif computer_choice > user_choice:
+    print("You lose!!")
+elif user_choice > computer_choice:
+    print("You win!!")
+elif user_choice == computer_choice:
+    print("It's a Draw!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
